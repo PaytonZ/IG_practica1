00191e250b2cf4d977090aeaaae6923de4b96273
own3dh2so4 <davgar04@ucm.es>
2013-10-21 16:03:06 +0200

    Payton gay

--- freeglut project 2D/main.cpp
+++ freeglut project 2D/main.cpp
@@ -72,31 +72,6 @@ void display(){
 	escena::getAVEInstance()->arbol_pitagoras->pintaCuadrados();
 	
 	
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-	cuadradoprincipal.pintarCuadrado();*/
-
-
-
-
-	escena::getAVEInstance()->arbol_pitagoras.pintaCuadrados();
-	
-	
-	/*glBegin(GL_QUADS);
-
-		glVertex2d(100,10);
-		glVertex2d(150,10);
-		glVertex2d(150,60);
-		glVertex2d(100,60);
-
-	glEnd();*/
->>>>>>> 5deea40d5f70b2a4e8c138d32bdf0b5006e81b70
->>>>>>> ded47e05e2e5d296f34ee78f56bf84a6ca2a21f2
->>>>>>> 9fd1f27fcf753cb90eb51381b14b69b5acebbe7a
 
 
 	glFlush();
